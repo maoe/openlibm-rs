bdaade92ef803a8e1205009e02d57941682fdd93
Mitsutoshi Aoe <me@maoe.name>
2021-03-25 15:43:33 +0900

    Pass -j option to make if NUM_JOBS exists

--- openlibm-src/build.rs
+++ openlibm-src/build.rs
@@ -3,10 +3,13 @@ use std::{env, path::PathBuf, process::Command};
 fn main() -> anyhow::Result<()> {
     let out_dir = PathBuf::from(env::var("OUT_DIR")?);
     let src_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?).join("upstream");
-    Command::new("make")
-        .arg("libopenlibm.a")
-        .current_dir(&src_dir)
-        .status()?;
+    let num_jobs = env::var("NUM_JOBS");
+    let mut build = Command::new("make");
+    build.arg("libopenlibm.a").current_dir(&src_dir);
+    if let Ok(j) = num_jobs {
+        build.arg(format!("-j{}", j));
+    }
+    build.status()?;
     Command::new("make")
         .arg(format!("prefix={}", out_dir.display()))
         .arg("install-static")
